4fe78fdef6a222bb341f4477d6e4d16caa04c7ba
Daniel Plaisted <dsplaisted@gmail.com>
2013-12-29 09:06:27 -0800

    Add dummy types for MSTest namespaces

---
+++ PCLTesting/DummyTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//  Create dummy types in MSTest namespaces so tests which are cross-compiled don't have to use #if's for using statements
+
+//  Namespace for Windows Store app MSTest attributes
+namespace Microsoft.VisualStudio.TestPlatform.UnitTestFramework
+{
+    class DummyType
+    {
+    }
+}
+
+//  Normal MSTest namespace
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    class DummyType
+    {
+    }
+}
