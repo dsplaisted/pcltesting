f439d06e5dd13c2b4a5b39686fec9cf5496255e9
Andrew Arnott <andrewarnott@gmail.com>
2014-04-15 22:39:01 -0700

    Adds Runner assembly as a separate entity. Adds a view model for the runner.

---
+++ PCLTesting.Runner/TestRunnerViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PCLCommandBase;
+using Validation;
+
+namespace PCLTesting.Infrastructure
+{
+    public class TestRunnerViewModel : BindableBase
+    {
+        private readonly TestRunner runner;
+
+        public TestRunnerViewModel(TestRunner runner)
+        {
+            this.runner = runner;
+            var startCommand = new StartCommandImpl(this);
+            this.StartCommand = startCommand;
+            this.StopCommand = new CancelCommand(startCommand);
+        }
+
+        private TestRunProgress currentProgress;
+        public TestRunProgress CurrentProgress
+        {
+            get { return this.currentProgress; }
+            set { this.SetProperty(ref this.currentProgress, value); }
+        }
+
+        public ICommand StartCommand { get; private set; }
+
+        public ICommand StopCommand { get; private set; }
+
+        private class StartCommandImpl : CommandBase
+        {
+            private readonly TestRunnerViewModel viewModel;
+
+            internal StartCommandImpl(TestRunnerViewModel viewModel)
+            {
+                Requires.NotNull(viewModel, "viewModel");
+                this.viewModel = viewModel;
+            }
+
+            protected override async Task ExecuteCoreAsync(object parameter, CancellationToken cancellationToken)
+            {
+                var progress = new Microsoft.Progress<TestRunProgress>(value => this.viewModel.CurrentProgress = value);
+                await this.viewModel.runner.RunTestsAsync(progress, cancellationToken);
+            }
+        }
+    }
+}
