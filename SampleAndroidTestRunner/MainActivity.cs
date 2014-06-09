9085843ab9a635bc7bd5706a5541b024036f2c54
Andrew Arnott <andrewarnott@gmail.com>
2014-06-09 08:00:11 -0700

    Adds sample Android test runner.

---
+++ SampleAndroidTestRunner/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿namespace SampleAndroidTestRunner
+{
+    using System;
+    using System.Reflection;
+    using Android.App;
+    using Android.Content;
+    using Android.OS;
+    using Android.Runtime;
+    using Android.Views;
+    using Android.Widget;
+    using PCLTesting.Forms;
+    using PCLTesting.Infrastructure;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.Android;
+
+    [Activity(Label = "SampleAndroidTestRunner", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AndroidActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var runner = new TestRunner(Assembly.GetExecutingAssembly());
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            SetPage(new NavigationPage(new TestRunnerPage(runner)));
+        }
+    }
+}
+
--- SampleWP8TestRunner/SampleTests.cs
+++ SampleWP8TestRunner/SampleTests.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using PCLTesting;
 
     [TestClass]
     public class SampleTests
